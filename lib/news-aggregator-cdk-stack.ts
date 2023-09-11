e246603e0fcbb751410f905d6fbb9378c32a270a
Kartheek, Gottipati <kartheekgottipati@gmail.com>
2023-09-10 20:16:12 -0400

    add news aggregrator stack

--- lib/news-aggregator-cdk-stack.ts
+++ lib/news-aggregator-cdk-stack.ts
@@ -4,6 +4,7 @@ import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as eventsources from 'aws-cdk-lib/aws-lambda-event-sources';
 
 export class NewsAggregatorCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,18 +14,49 @@ export class NewsAggregatorCdkStack extends cdk.Stack {
       partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      stream: dynamodb.StreamViewType.NEW_IMAGE,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
-    const feedManagerLambda = new cdk.aws_lambda.Function(
+    const feedManagerLambda = new lambda.Function(this, 'FeedManagerLambda', {
+      description: 'Lambda function to manage feeds',
+      runtime: lambda.Runtime.PYTHON_3_10,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(
+        path.join(__dirname, '../lambdas/python/feed_manager'),
+        {
+          bundling: {
+            image: lambda.Runtime.PYTHON_3_10.bundlingImage,
+            command: [
+              'bash',
+              '-c',
+              'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'
+            ]
+          }
+        }
+      ),
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        TABLE_NAME: feedTable.tableName
+      }
+    });
+
+    feedManagerLambda.addEnvironment(
+      'POWERTOOLS_METRICS_NAMESPACE',
+      'ChainProfile/NewsFeedAggregator/FeedManager'
+    );
+    feedManagerLambda.addEnvironment('POWERTOOLS_SERVICE_NAME', 'FeedManager');
+
+    const feedItemCreatorLambda = new lambda.Function(
       this,
-      'FeedManagerLambda',
+      'FeedItemCreatorLambda',
       {
-        description: 'Lambda function to manage feeds',
+        description: 'Lambda function to create feed items',
         runtime: lambda.Runtime.PYTHON_3_10,
-        handler: 'index.handler',
+        handler: 'index.stream_handler',
         code: lambda.Code.fromAsset(
-          path.join(__dirname, '../lambdas/python/feed_manager'),
+          path.join(__dirname, '../lambdas/python/feed_item_creator'),
           {
             bundling: {
               image: lambda.Runtime.PYTHON_3_10.bundlingImage,
@@ -44,13 +76,59 @@ export class NewsAggregatorCdkStack extends cdk.Stack {
       }
     );
 
-    feedManagerLambda.addEnvironment(
-      'POWERTOOLS_METRICS_NAMESPACE',
-      'ChainProfile/NewsFeedAggregator/FeedManager'
+    const webSubSubscriptionLambda = new lambda.Function(
+      this,
+      'WebSubSubscriptionLambda',
+      {
+        description: 'Lambda function to subscribe to web sub feeds',
+        runtime: lambda.Runtime.PYTHON_3_10,
+        handler: 'index.handler',
+        code: lambda.Code.fromAsset(
+          path.join(__dirname, '../lambdas/python/web_sub_subscription'),
+          {
+            bundling: {
+              image: lambda.Runtime.PYTHON_3_10.bundlingImage,
+              command: [
+                'bash',
+                '-c',
+                'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'
+              ]
+            }
+          }
+        ),
+        architecture: lambda.Architecture.ARM_64,
+        timeout: cdk.Duration.seconds(10),
+        environment: {
+          TABLE_NAME: feedTable.tableName
+        }
+      }
     );
-    feedManagerLambda.addEnvironment('POWERTOOLS_SERVICE_NAME', 'FeedManager');
 
     feedTable.grantReadWriteData(feedManagerLambda);
+    feedTable.grantReadWriteData(feedItemCreatorLambda);
+    feedTable.grantReadWriteData(webSubSubscriptionLambda);
+
+    feedItemCreatorLambda.addEventSource(
+      new eventsources.DynamoEventSource(feedTable, {
+        startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+        batchSize: 10,
+        bisectBatchOnError: true,
+        retryAttempts: 10
+      })
+    );
+
+    feedTable.grantStreamRead(feedItemCreatorLambda);
+
+    webSubSubscriptionLambda.addEventSource(
+      new eventsources.DynamoEventSource(feedTable, {
+        startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+        batchSize: 10,
+        bisectBatchOnError: true,
+        retryAttempts: 10
+      })
+    );
+
+    feedTable.grantStreamRead(webSubSubscriptionLambda);
 
     const feedManagerApi = new apigateway.RestApi(this, 'FeedManagerApi', {
       restApiName: 'Feed Manager API',
@@ -66,21 +144,6 @@ export class NewsAggregatorCdkStack extends cdk.Stack {
 
     // POST /feeds - Adds a new feed
     feedManagerResource.addMethod('POST', feedManagerLambdaIntegration);
-
-    // GET /feeds - Retrieves a list of active feeds
-    feedManagerResource.addMethod('GET', feedManagerLambdaIntegration);
-
-    // Create a resource for specific feeds: /feeds/{feed_id}
-    const singleFeedResource = feedManagerResource.addResource('{feed_id}');
-
-    // DELETE /feeds/{feed_id} - Marks a feed as inactive or soft-deletes it
-    singleFeedResource.addMethod('DELETE', feedManagerLambdaIntegration);
-
-    // Create a resource for items of specific feeds: /feeds/{feed_id}/items
-    const itemsResource = singleFeedResource.addResource('items');
-
-    // GET /feeds/{feed_id}/items - Retrieves items for a specific feed
-    itemsResource.addMethod('GET', feedManagerLambdaIntegration);
 
     new cdk.CfnOutput(this, 'FeedManagerApiUrl', {
       value: feedManagerApi.url
